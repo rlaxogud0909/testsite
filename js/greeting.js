333eb20efe6e46b1db86f3a1a136dbaccb90a033
rlaxogud0909 <63087652+rlaxogud0909@users.noreply.github.com>
2023-05-28 02:26:56 +0900

    Add files via upload

---
+++ js/greeting.js
@@ -0,0 +1,37 @@
+const greetingScreen = document.getElementById("greeting-screen");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginPlaceholder = loginForm.querySelector("span");
+const greeting = document.getElementById("greeting");
+
+const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginForm.style.display = "none";
+
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
+}
+
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `${username}님 환영합니다`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+    setTimeout(() => {
+        greetingScreen.animate({ opacity: 0, visibility: "hidden" }, { duration: 500, fill: "forwards" });
+    }, 2500);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings();
+}
